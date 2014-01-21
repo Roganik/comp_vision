d970ffa2a91d334372a8cc2338b4040ed484f5a0
Nickolay Rogalskiy <roganik@gmail.com>
2014-01-21 21:10:29 +0300

    fix indexes

--- practicum1.py
+++ practicum1.py
@@ -9,20 +9,20 @@ def partial_differential(i,a):
 	global m
 
 	if   a == "d/dx":
-		for k1 in range(n-1):
-			for k2 in range(m):
+		for k1 in range(m-1):
+			for k2 in range(n):
 				i[k1][k2] = i[k1+1][k2] - i[k1][k2]
 	elif a == "d/dy":
-		for k1 in range(n):
-			for k2 in range(m-1):
+		for k1 in range(m):
+			for k2 in range(n-1):
 				i[k1][k2] = i[k1][k2+1] - i[k1][k2]
 	elif a == "d^2/dx^2":
-		for k1 in range(n-2):
-                        for k2 in range(m):
+		for k1 in range(m-2):
+                        for k2 in range(n):
                                 i[k1][k2] = i[k1+2][k2] - 2 * i[k1+1][k2] + i[k1][k2]
 	elif a == "d^2/dy^2":
-		for k1 in range(n):
-                        for k2 in range(m-2):
+		for k1 in range(m):
+                        for k2 in range(n-2):
                                 i[k1][k2] = i[k1][k2+2] - 2 * i[k1][k2+1] + i[k1][k2]
 	else:
 		print("incorrect input")
@@ -31,16 +31,16 @@ def partial_differential(i,a):
 # Определить массив в котором хранить изображение
 # Может состоять из (0...1), (0...255), ([0...255], [0...255], [0...255])
 
-n = 20
-m = 10
+n = 20 #X
+m = 10 #Y
 
 i = []
 
-# Случайное ч/б изображение
+# Случайное ч/б изображение 20x10
 
-for k1 in range(n):
+for k1 in range(m):
 	i.append([])
-	for k2 in range(m):
+	for k2 in range(n):
 		i[k1].append(random.randint(0,1))
 
 # Описать Convolution изображения с ядром k
