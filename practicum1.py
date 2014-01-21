e6bc9c3fe8757dc4b5f43e9dd31a6df10b5963f9
Nickolay Rogalskiy <roganik@gmail.com>
2014-01-21 20:44:42 +0300

    add function partial_differential

--- practicum1.py
+++ practicum1.py
@@ -2,6 +2,32 @@
 
 import random
 
+# Partial differentials
+
+def partial_differential(i,a):
+	global n
+	global m
+
+	if   a == "d/dx":
+		for k1 in range(n-1):
+			for k2 in range(m):
+				i[k1][k2] = i[k1+1][k2] - i[k1][k2]
+	elif a == "d/dy":
+		for k1 in range(n):
+			for k2 in range(m-1):
+				i[k1][k2] = i[k1][k2+1] - i[k1][k2]
+	elif a == "d^2/dx^2":
+		for k1 in range(n-2):
+                        for k2 in range(m):
+                                i[k1][k2] = i[k1+2][k2] - 2 * i[k1+1][k2] + i[k1][k2]
+	elif a == "d^2/dy^2":
+		for k1 in range(n):
+                        for k2 in range(m-2):
+                                i[k1][k2] = i[k1][k2+2] - 2 * i[k1][k2+1] + i[k1][k2]
+	else:
+		print("incorrect input")
+
+	return i
 # Определить массив в котором хранить изображение
 # Может состоять из (0...1), (0...255), ([0...255], [0...255], [0...255])
 
@@ -16,8 +42,6 @@ for k1 in range(n):
 	i.append([])
 	for k2 in range(m):
 		i[k1].append(random.randint(0,1))
-
-# Описать частные дифференциалы: d/dx, d/dy, d^2/dx^2, d^2/dy^2
 
 # Описать Convolution изображения с ядром k
 
