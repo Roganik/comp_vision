5a89f226aa76a982ff5436c3c1ce77275a4f8397
Nickolay Rogalskiy <roganik@gmail.com>
2014-01-24 00:09:05 +0300

    add two dimensional convolution
    Example from "http://www.songho.ca/dsp/convolution/convolution2d_example.html":
    	i = [[1,2,3],[4,5,6],[7,8,9]]
    	kern = [[-1,-2,-1],[0,0,0],[1,2,1]]
    	print(convolution(i,kern))

--- practicum1.py
+++ practicum1.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import random
+import numpy
 
 # Partial differentials
 
@@ -26,9 +27,49 @@ def partial_differential(i,a):
 
 	return i
 
-# Full Convolution of image I with kernel K
 
 def convolution(i,kernel):
+	tmp = []
+	try:
+		len(kernel[0])
+	except IndexError:
+		tmp = [-1]
+
+	if tmp == [-1]:
+		tmp = convolution_dimensional(i,kernel)
+	else:
+		tmp = convolution_two_dimensional(i,kernel)
+	return tmp
+
+def convolution_two_dimensional(i,kernel):
+#                m   n
+# (k*l)*[x,y] = SUM SUM k[i,j] * l[x-i,y-j]
+#               j=1 i
+	kernel_centerY = len(kernel) / 2
+	kernel_centerX = len(kernel[0]) / 2
+	sum = 0
+
+	i2 = []
+	for k1 in range(len(i)):
+		i2.append([])
+		for k2 in range(len(i[0])):
+			sum = 0
+			for k3 in range(len(kernel)):
+				n = len(kernel) - 1 - k3
+				for k4 in range(len(kernel[0])):
+					m = len(kernel[0]) - 1 - k4
+					jj = k1 + (k3 - kernel_centerY)
+					ii = k2 + (k4 - kernel_centerX)
+					if ((ii >= 0) and (ii < len(i)) and (jj >=0) and (jj < len(i[0]))):
+						sum += i[ii][jj] * kernel[m][n]
+			i2[k1].append(sum)
+	i3 = numpy.array(i2)
+	i3 = i3.T
+	return i3
+
+# Full Convolution of image I with kernel K
+
+def convolution_dimensional(i,kernel):
 	summa = []
 	all_summs = []
 	sum = 0
