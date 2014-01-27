f830891e6f6780943f984fd935a7193762464655
Nickolay Rogalskiy <roganik@gmail.com>
2014-01-27 15:02:19 +0300

    remove unnecessary unused code

--- practicum1.py
+++ practicum1.py
@@ -2,8 +2,6 @@
 
 import random
 
-# Partial differentials
-
 def partial_differential(i,a):
 	if   a == "d/dx":
 		for k1 in range(m-1):
@@ -26,21 +24,7 @@ def partial_differential(i,a):
 
 	return i
 
-
 def convolution(i,kernel):
-	tmp = []
-	try:
-		len(kernel[0])
-	except IndexError:
-		tmp = [-1]
-
-	if tmp == [-1]:
-		tmp = convolution_dimensional(i,kernel)
-	else:
-		tmp = convolution_two_dimensional(i,kernel)
-	return tmp
-
-def convolution_two_dimensional(i,kernel):
 #                m   n
 # (k*l)*[x,y] = SUM SUM k[i,j] * l[x-i,y-j]
 #               j=1 i
@@ -64,45 +48,6 @@ def convolution_two_dimensional(i,kernel):
 			i2[k1].append(sum)
 	return i2
 
-# Full Convolution of image I with kernel K
-
-def convolution_dimensional(i,kernel):
-	summa = []
-	all_summs = []
-	sum = 0
-	kernel.reverse()
-	for k1 in range(len(i)):
-		for k2 in range(len(kernel)-1):
-			i[k1].insert(0,0)
-			i[k1].append(0)
-		for k2 in range(len(i[k1])-len(kernel)+1):
-			for k3 in range(len(kernel)):
-				sum = kernel[k3]*i[k1][k3+k2] + sum
-			summa.append(sum)
-			sum = 0
-		all_summs.append(summa)
-		summa = []
-	return all_summs
-
-n = 20 #X
-m = 10 #Y
-
-i = []
-
-# Случайное ч/б изображение 20x10
-
-for k1 in range(m):
-	i.append([])
-	for k2 in range(n):
-		i[k1].append(random.randint(0,1))
-
-# Описать частные производные: d/dx, d/dy, d^2/dx^2, d^2/dy^2
-
-# Примеры convolution: sobel operator, box filter (blur), gaussian blur
-#		       sharpen, unsharp mask
-
-# Описать интегралл изображения	
-
 if __name__ == "__main__":
 	tutorial2matrix = [[14, 7, 7, 6, 15, 20, 10],
 	                   [14, 9, 13, 11, 2, 20, 2],
@@ -115,6 +60,6 @@ if __name__ == "__main__":
 	tutorial2kernel2 = [[1, 0], [0, 1]]
 	tutorial2kernel3 = [[1, 2, 3], [0, 0, -2], [5, 7, 11], [2, 3, 1]]
 	print "Tutorial 2 convolution"
-	print convolution_two_dimensional(tutorial2matrix, tutorial2kernel1) # :(
-	print convolution_two_dimensional(tutorial2matrix, tutorial2kernel2) # :D
-	print convolution_two_dimensional(tutorial2matrix, tutorial2kernel3) # :'(
+	print convolution(tutorial2matrix, tutorial2kernel1),"\n"
+	print convolution(tutorial2matrix, tutorial2kernel2),"\n"
+	print convolution(tutorial2matrix, tutorial2kernel3),"\n"
